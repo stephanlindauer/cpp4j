198e32305ce73f20321b25659295ab0a6343c0c4
Kai Nessig <k.nessig@gmx.de>
2012-04-10 19:24:48 +0200

    first implementations

--- cpp4j/rationalnumberarray.cpp
+++ cpp4j/rationalnumberarray.cpp
@@ -1,8 +1,63 @@
+#include "stdlib.h"
 #include "rationalnumberarray.h"
 
 
 struct RationalNumberArray{
-   RationalNumber *data;
-   int size;
-   int capacity;
+    RationalNumber *data;
+    int size;
+    int capacity;
 };
+
+const RationalNumber nullRationalNumber = { 0, 1 };
+
+
+RationalNumberArray* rnaCreate(const unsigned int size) {
+    RationalNumberArray * rna = (RationalNumberArray*) malloc (
+                sizeof (RationalNumber *) +
+                sizeof (int) +
+                sizeof (int));
+
+    rna->data = (RationalNumber*) malloc (sizeof (RationalNumber) * size);
+
+    for (unsigned int i = 0; i < size; i++) {
+        rna->data[i] = nullRationalNumber;
+    };
+
+    rna->size = 0;
+    rna->capacity = size;
+
+    return rna;
+}
+
+void rnaDelete(RationalNumberArray * rna) {
+    return;
+}
+
+void rnaResize(const RationalNumberArray * rna, const unsigned int newSize) {
+    return;
+}
+
+unsigned int rnaSize(const RationalNumberArray * rna) {
+    return rna->size;
+}
+
+unsigned int rnaCapacity(const RationalNumberArray * rna) {
+    return rna->capacity;
+}
+
+void rnaAdd(const RationalNumberArray * rna, const RationalNumber newRationalNumber) {
+    return;
+}
+
+void rnaSet(const RationalNumberArray * rna, const RationalNumber rationalNumber, const unsigned int position) {
+    return;
+}
+
+RationalNumber rnaGet(const RationalNumberArray * rna, const unsigned int position) {
+    RationalNumber rn;
+    return rn;
+}
+
+void rnaRemove(const RationalNumberArray * rna, const unsigned int from, const unsigned int to) {
+    return;
+}
