fafc0fb311d0c703dabc173d0f498833533c775c
tiberius <tiberius@ti-mobile.(none)>
2012-06-01 12:53:54 +0200

    added template testfile and included it in main

--- cpp4j/testMain.cpp
+++ cpp4j/testMain.cpp
@@ -6,6 +6,7 @@
 #include "testRNA.cpp"
 #include "testCPP_RN.cpp"
 #include "testCPP_RNA.cpp"
+#include "testTemplates.cpp"
 
 int main()
 {
@@ -14,6 +15,7 @@ int main()
     testRNA();
     testCPP_RN();
     testCPP_RNA();
+    testTemplates();
     printf( "tests done \n");
 
     return 0;
